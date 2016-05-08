326d8483fbc77abecd939834ff905cf48ec73c7c
Arshad Khan <arshdkhn1@gmail.com>
2016-05-08 19:50:20 +0530

    corrected js errors

--- portfolio-freecodecamp/js/index.js
+++ portfolio-freecodecamp/js/index.js
@@ -257,57 +257,57 @@ window.onload = function() {
 	var elevator1 = new Elevator({
     element: document.querySelector('.elevator-button1'),
     targetElement: document.querySelector('#elevator-target1'),
-    mainAudio: '../music/elevator.mp3',
-    endAudio: '../music/end.mp3'
+    mainAudio: 'music/elevator.mp3',
+    endAudio: 'music/end.mp3'
   });
 
 	 var elevator2 = new Elevator({
     element: document.querySelector('.elevator-button2'),
     targetElement: document.querySelector('#elevator-target2'),
-    mainAudio: '../music/elevator.mp3',
-    endAudio: '../music/end.mp3';
+    mainAudio: 'music/elevator.mp3',
+    endAudio: 'music/end.mp3'
   });
 
 	var elevator3 = new Elevator({
     element: document.querySelector('.elevator-button3'),
     targetElement: document.querySelector('#elevator-target3'),
-    mainAudio: '../music/elevator.mp3',
-    endAudio: '../music/end.mp3';
+    mainAudio: 'music/elevator.mp3',
+    endAudio: 'music/end.mp3'
   });
 
 	var elevator4 = new Elevator({
     element: document.querySelector('.elevator-button4'),
     targetElement: document.querySelector('#elevator-target4'),
-    mainAudio: '../music/elevator.mp3',
-    endAudio: '../music/end.mp3';
+    mainAudio: 'music/elevator.mp3',
+    endAudio: '.music/end.mp3'
   });
 
 	var elevator5 = new Elevator({
     element: document.querySelector('.elevator-button5'),
     targetElement: document.querySelector('#elevator-target5'),
-    mainAudio: '../music/elevator.mp3',
-    endAudio: '../music/end.mp3';
+    mainAudio: 'music/elevator.mp3',
+    endAudio: 'music/end.mp3'
   });
 
 	var elevator6 = new Elevator({
     element: document.querySelector('.elevator-button6'),
     targetElement: document.querySelector('#elevator-target6'),
-    mainAudio: '../music/elevator.mp3',
-    endAudio: '../music/end.mp3';
+    mainAudio: 'music/elevator.mp3',
+    endAudio: 'music/end.mp3'
   });
 
 	var elevator7 = new Elevator({
     element: document.querySelector('.elevator-button7'),
     targetElement: document.querySelector('#elevator-target7'),
-    mainAudio: '../music/elevator.mp3',
-    endAudio: '../music/end.mp3';
+    mainAudio: 'music/elevator.mp3',
+    endAudio: 'music/end.mp3'
   });
 
 	var elevator8 = new Elevator({
     element: document.querySelector('.elevator-button8'),
     targetElement: document.querySelector('#elevator-target8'),
-    mainAudio: '../music/elevator.mp3',
-    endAudio: '../music/end.mp3';
+    mainAudio: 'music/elevator.mp3',
+    endAudio: 'music/end.mp3'
   });
 
 }
