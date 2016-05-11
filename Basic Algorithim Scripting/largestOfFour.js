8639c16b65f668ddd4c181e814ff63745d9fd0ff
Arshad Khan <arshdkhn1@gmail.com>
2016-05-11 22:06:29 +0530

    Basic algorithm scripting problems

---
+++ Basic Algorithim Scripting/largestOfFour.js
@@ -0,0 +1,14 @@
+
+function largestOfFour(arr) {
+  for(var elem in arr){
+
+   arr[elem] = arr[elem].reduce(function(a, b){
+      return a > b ? a:b;
+
+    });
+
+  }
+  return arr;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
