a37c61cd4919ff5b492a469b2c0b0ccbedc66d94
Arshad Khan <arshdkhn1@gmail.com>
2016-05-13 00:02:28 +0530

    styling and quotes functionality done

--- random-quotes-machine-freecodecamp/js/index.js
+++ random-quotes-machine-freecodecamp/js/index.js
@@ -3,14 +3,18 @@ $(document).ready(function() {
   $("#next").on("click",function(){
     getRandomJokes();
   });
+
 });//ends document.ready
 
 function getRandomJokes(){
-    $.getJSON("http://api.icndb.com/jokes/random",function(data){
-      $("#writer").html(data["value"].joke);
-      $("#quote").html(data["value"].joke);
-      var tweetUrl ="https://twitter.com/intent/tweet?text=" + data["value"].joke;
-   // $("body").css("background-image","url('https://source.unsplash.com/random')");
-      $("#tweetAnchor").attr("href",tweetUrl);
-    });
+    $.getJSON("http://api.icndb.com/jokes/random",
+           function(data){
+    $("#writer").html(data["value"].joke);
+    $("#quote").html(data["value"].joke);
+
+    var tweetUrl ="https://twitter.com/intent/tweet?text=" + data["value"].joke;
+   //$("body").css("background-image","url('https://source.unsplash.com/random')");
+    $("#tweetAnchor").attr("href",tweetUrl);
+  });
+
 }
