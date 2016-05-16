bbf00ac4a39003027ee9fcd9e962796b1d2c90de
Arshad Khan <arshdkhn1@gmail.com>
2016-05-17 03:56:48 +0530

    editing

--- wikipedia-viewer-freecodecamp/js/index.js
+++ wikipedia-viewer-freecodecamp/js/index.js
@@ -1,5 +1,61 @@
 $(document).ready(function(){
-  $(".glyphicon-search").on("click",function(){
+  var keyword = "";
+  var resultArea = $("#results");
+  var searchBar = $("#searchBar");
+  var searchButton = $(".glyphicon-search");
+  var searchUrl = "https://en.wikipedia.org/w/api.php";
+  var displayResults = function(){
+  $.ajax({
+      url: searchUrl,
+      dataType: 'jsonp',
+      data: {
+        action: 'query',
+        format: 'json',
+        generator: 'search',
+          gsrsearch: keyword,
+          gsrnamespace: 0,
+          gsrlimit: 10,
+        prop:'extracts|pageimages',
+          exchars: 200,
+          exlimit: 'max',
+          explaintext: true,
+          exintro: true,
+          piprop: 'thumbnail',
+          pilimit: 'max',
+          pithumbsize: 200
+      },
+      success: function(json){
+        var results = json.query.pages;
+        $.map(results, function(result){
+          var elem = $('<li>');
+          elem.append($('<h3>').text(result.title));
+          if(result.thumbnail) elem.append($('<img>').attr('width',150).attr('src',result.thumbnail.source));
+          elem.append($('<p>').text(result.extracts));
+          resultArea.append(elem);
+        });
+      }
+    });
+  };
+  /*
+  searchBar.autocomplete({s
+        source: function (request, response) {
+            $.ajax({
+                url: searchUrl,
+                dataType: 'jsonp',
+                data: {
+                    'action': "opensearch",
+                    'format': "json",
+                    'search': request.term
+                },
+                success: function (data) {
+                    response(data[1]);
+                }
+            });
+        }
+    });*/
+  searchButton.click(function(){
+    keyword = searchBar.val();
     $(".container-fluid").animate({height:"30vh"}, 600);
+    displayResults();
   });
 });
