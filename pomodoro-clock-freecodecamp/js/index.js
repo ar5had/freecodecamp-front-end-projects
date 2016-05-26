491a96aa4a8c3599d03dc9a52a1b0030490c459b
Arshad Khan <arshdkhn1@gmail.com>
2016-05-27 04:21:30 +0530

    added minimum and max limits

--- pomodoro-clock-freecodecamp/js/index.js
+++ pomodoro-clock-freecodecamp/js/index.js
@@ -1,25 +1,25 @@
 $(document).ready(function(){
   var inc = $(".inc");
   var dec = $(".dec");
-  
+
   inc.on("click",function(){
     var elem = $(this).prev();
     var value = parseInt(elem.text());
-    value++;
+    value = Math.min(99, value+1);
     elem.text(value);
     if(elem.hasClass("sl")){
       $("#sesTimer").text(value);
     }
   });
-  
+
   dec.on("click",function(){
     var elem = $(this).next();
     var value = parseInt(elem.text());
-    value--;
+    value = Math.max(0, value-1);
     elem.text(value);
     if(elem.hasClass("sl")){
       $("#sesTimer").text(value);
     }
   });
-  
+
 });
