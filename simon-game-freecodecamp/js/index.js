501eec28587215cc3eaf027cdc703d7ad36d2705
Arshad Khan <arshdkhn1@gmail.com>
2016-06-01 15:32:16 +0530

    simon game program done!

--- simon-game-freecodecamp/js/index.js
+++ simon-game-freecodecamp/js/index.js
@@ -2,15 +2,12 @@ $(document).ready(function(){
   var on = false;
   var start = false;
   var strict = false;
-  var score = 0;
-
-
-//////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////
-//////////////control buttons setup starts//////////////
-///////////////////////////////////////////////////////
-//////////////////////////////////////////////////////
-
+  var score = 1;
+  var count = 0;
+  var glowButtonClickable = false;
+  var clickCount = 0;
+  var glowButtons = [], clickedButtons = [];
+  var timeout;
 
   $(".onOff").click(function(){
     if(!on){
@@ -23,6 +20,7 @@ $(document).ready(function(){
       $(".display > h5").css({'padding-top': "0"});
     }
     else{
+      clearGame();
       on = false;
       $(this).text("On");
       $(".display > h5").css({'padding-top': "30rem"});
@@ -44,15 +42,16 @@ $(document).ready(function(){
         $(this).text("Stop");
         $(".strict").addClass("unclickable");
         start = true;
-        startGame();
+        setTimeout(function(){startGame();},1200);
       }
       else{
+        clearGame();
+        updateScore();
         $(".strict").removeClass("unclickable");
         $(this).text("Start");
         start = false;
         $(".strict").text("strict");
         strict = false;
-        pattern = [];
       }
     }
   });
@@ -69,18 +68,97 @@ $(document).ready(function(){
     }
   });
 
+  function startGame(){
+    glowButtons.push( String( (Math.floor(Math.random()* 10 + 1) % 4) + 1 ) );
+    glow();
+  }
 
-//////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////
-//////////////control buttons setup ended///////////////
-///////////////////////////////////////////////////////
-//////////////////////////////////////////////////////
+  function glow(){
+      var str = ".button" + glowButtons[count];
+      $(str).addClass("glow").delay(1000).queue(function(){
+        count++;
+        $(this).removeClass("glow").dequeue();
+        if(count < glowButtons.length){
+          setTimeout(function(){
+              glow();
+          },900);
+        }
+        else{
+          count = 0;
+          glowButtonClickable = true;
+        }
+      });
+  }
 
+  $(".button").click(function(){
+    if(glowButtonClickable){
+      clickCount++;
+      clickedButtons.push($(this).attr('data-pos'));
+      if(clickedButtons[clickCount-1] !== glowButtons[clickCount-1]){
+        showStatus();
+        if(strict)
+          clearGame();
+        else
+          giveAnotherChance();
+        return;
+      }
+      if(clickedButtons.length === glowButtons.length){
+        glowButtonClickable = false;
+        clickCount = 0;
+        clickedButtons = [];
+        score++;
+        updateScore();
+        if(score !== 21){
+          timeout = setTimeout(function(){
+            startGame();
+          }, 1000);
+        }
+        else
+          showVictory();
+      }
+    }
+  });
 
-//////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////
-////////////// ///////////////
-///////////////////////////////////////////////////////
-//////////////////////////////////////////////////////
+  function clearGame(){
+    glowButtonClickable = false;
+    clickCount = 0;
+    score = 1;
+    count = 0;
+    glowButtons = [];
+    clickedButtons = [];
+    $(".start").text("start");
+    start = false;
+    updateScore()
+  }
 
+  function giveAnotherChance(){
+    glowButtonClickable = false;
+    clickCount = 0;
+    clickedButtons = [];
+    setTimeout(function(){
+      glow();
+    }, 1500);
+  }
+
+  function showStatus(){
+    $(".count").text("!!!!!!!!!").css({color:"red"});
+    $(".score").text("");
+    setTimeout(function(){
+      $(".count").text("Count : ").css({color:"#EEE"});
+      $(".score").text(score);
+    },1200);
+  }
+
+  function updateScore(){
+
+    $(".count").text("Count : ").css({color:"#EEE"});
+    $(".score").text(score);
+
+  }
+
+  function showVictory(){
+      $(".score").text("");
+      $(".count").text("You Win!!!").css({color:"green"});
+      setTimeout(function(){clearGame();}, 2000);
+  }
 });
