211bf50afdabf16ab02080acd34a7e9fd053e169
Arshad Khan <arshdkhn1@gmail.com>
2016-06-01 10:45:20 +0530

    editing

--- simon-game-freecodecamp/js/index.js
+++ simon-game-freecodecamp/js/index.js
@@ -3,22 +3,38 @@ $(document).ready(function(){
   var start = false;
   var strict = false;
   var score = 0;
+
+
+//////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+//////////////control buttons setup starts//////////////
+///////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+
+
   $(".onOff").click(function(){
     if(!on){
+      $(".strict").removeClass("unclickable");
+      $(".start").removeClass("unclickable");
       on = true;
       $(this).text("off");
       $(".count").text("Count :");
       $(".score").text(score);
+      $(".display > h5").css({'padding-top': "0"});
     }
     else{
       on = false;
       $(this).text("On");
+      $(".display > h5").css({'padding-top': "30rem"});
       $(".count").text("");
       $(".score").text("");
       $(".strict").text("strict");
       strict = false;
       $(".start").text("start");
       start = false;
+      $(".strict").addClass("unclickable");
+      $(".start").addClass("unclickable");
+      pattern = [];
     }
   });
 
@@ -26,14 +42,17 @@ $(document).ready(function(){
     if(on){
       if(!start){
         $(this).text("Stop");
+        $(".strict").addClass("unclickable");
         start = true;
-      //random no thing...
+        startGame();
       }
       else{
+        $(".strict").removeClass("unclickable");
         $(this).text("Start");
         start = false;
         $(".strict").text("strict");
         strict = false;
+        pattern = [];
       }
     }
   });
@@ -46,7 +65,22 @@ $(document).ready(function(){
     else if(strict && !start && on){
       $(this).text("Strict");
       strict = false;
+      pattern = [];
     }
   });
+
+
+//////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+//////////////control buttons setup ended///////////////
+///////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+////////////// ///////////////
+///////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
 
 });
